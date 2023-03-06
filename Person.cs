7f082b70f75b021f0e56aa53c27eb94efc54aace
Dawid Dziewic <w64071@student.wsiz.edu.pl>
2023-03-06 14:15:10 +0100

    Add new models

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public Address Address { get; set; }
+    }
+
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,7 +8,9 @@ namespace Lab1
         [Fact]
         public void Test1()
         {
-
+            int i = 0;
+            i++;
+            Assert.True(true);
         }
     }
 }
